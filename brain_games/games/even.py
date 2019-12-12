922976dd9c529693b7514320b36073cac61e37bf
Roman <fcdm@inbox.ru>
2019-12-12 20:50:34 +0200

    stage 5 is ready

---
+++ brain_games/games/even.py
@@ -0,0 +1,34 @@
+import random
+import prompt
+
+
+def is_even(num):
+    if num % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    print('Answer "yes" if number even otherwise answer "no". \n')
+    name = prompt.string('May I have your name? ')
+    print('Hello, {}! \n'.format(name))
+    counter = 0
+    while counter < 3:
+        guess = random.randint(1, 100)
+        print('Question: ' + str(guess))
+        print('Your answer:', end=" ")
+        answer = input()
+        if answer == is_even(guess):
+            print('Correct!')
+            counter += 1
+        else:
+            print("'{}' is a wrong answer ;(. Correct answer was '{}'".format(answer, is_even(guess)))
+            break
+    if counter == 3:
+        print('Congratulations, {}!'.format(name))
+
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,9 @@
+import brain_games.games.even
+
+
+def main():
+    brain_games.games.even.main()
+
+
+if __name__ == '__main__':
+    main()
